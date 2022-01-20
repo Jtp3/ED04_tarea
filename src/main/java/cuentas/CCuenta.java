07485785afa07ff31534698a2ac3c923efb59982
“jtp1” <ue266083@edu.xunta.gal>
2022-01-20 11:23:38 +0000

    Tarea ED04 Segundo commit

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,8 +1,15 @@
 package cuentas;
+/**
+ * Se trata de una clase que describe una cuenta bancaria.
+ * Se registran moviemientos, saldos interés  
+  * @version 1.0, 20/01/22
+ * @author Ed04_Tarea
+ */
 
 public class CCuenta {
 
     /**
+     
      * @return the nombre
      */
     public String getNombre() {
@@ -67,26 +74,42 @@ public class CCuenta {
     public CCuenta()
     {
     }
-
+/**
+     * Crea CCuenta con los parámetros Nom, cue, sal y tipo
+     
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+/**
+ * Establece el estado de la cuenta con el saldo
+     *return Saldo
+     */
     public double estado()
     {
         return getSaldo();
     }
-
+/**
+     * Metodo ingresar
+     * @throws Exception Evita que se pueda ingresar cantidades en negativo
+     * @return Saldo
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+/**
+     * Metodo retirar
+     * Evita que se pueda retirar cantidades en negativo
+     * 
+     *  @throws Exception .Evita que se pueda retirar más cantidad de la existente en cuenta.
+     * 
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
